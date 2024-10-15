1c69a225533e3fe6eda66ae83ff9d2f03ef48f7d
lijiamin <lijiamin@nucarf.com>
2024-10-15 13:49:48 +0800

    feat: eslint 配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,39 @@
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
-// eslint-disable-next-line no-undef
-module.exports = {
+
+// import { configConventional } from '@commitlint/config-conventional'
+
+// export default {
+//     extends: ['@commitlint/config-conventional']
+// };
+export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        // @see: https://commitlint.js.org/#/reference-rules
+        'body-leading-blank': [1, 'always'],
+        'body-max-line-length': [2, 'always', 100],
+        'footer-leading-blank': [1, 'always'],
+        'footer-max-line-length': [2, 'always', 100],
+        'header-max-length': [2, 'always', 100],
+        'scope-case': [2, 'always', 'lower-case'],
+        'subject-case': [0],
+        'subject-empty': [0, 'never'],
+        'subject-full-stop': [0, 'never', '.'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'test',
+                'refactor',
+                'chore',
+                'revert',
+            ],
+        ],
     },
     prompt: {
         alias: { fd: 'docs: fix typos' },
